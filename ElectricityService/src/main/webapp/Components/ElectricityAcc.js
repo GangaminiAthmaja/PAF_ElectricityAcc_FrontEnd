f91e69ec1551eb5d48688dcdca63fc5b1686632c
Gangamini Athmaja <89349666+GangaminiAthmaja@users.noreply.github.com>
2022-05-14 22:33:30 +0530

    initial commit created project structure

---
+++ ElectricityService/src/main/webapp/Components/ElectricityAcc.js
@@ -0,0 +1,143 @@
+
+$(document).ready(function() 
+{ 
+if ($("#alertSuccess").text().trim() == "") 
+ { 
+ $("#alertSuccess").hide(); 
+ } 
+ $("#alertError").hide(); 
+}); 
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event) 
+{ 
+// Clear alerts---------------------
+ $("#alertSuccess").text(""); 
+ $("#alertSuccess").hide(); 
+ $("#alertError").text(""); 
+ $("#alertError").hide(); 
+// Form validation-------------------
+var status = validateItemForm(); 
+if (status != true) 
+ { 
+ $("#alertError").text(status); 
+ $("#alertError").show(); 
+ return; 
+ } 
+// If valid------------------------
+var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT"; 
+ $.ajax( 
+ { 
+ url : "ElectricityAccAPI", 
+ type : type, 
+ data : $("#formElectricityAcc").serialize(), 
+ dataType : "text", 
+ complete : function(response, status) 
+ { 
+ onAccountSaveComplete(response.responseText, status); 
+ } 
+ }); 
+});
+
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function(event) 
+{ 
+$("#hidItemIDSave").val($(this).data("accountID")); 
+ $("#accName").val($(this).closest("tr").find('td:eq(0)').text()); 
+ $("#accNumber").val($(this).closest("tr").find('td:eq(1)').text()); 
+ $("#premisesID").val($(this).closest("tr").find('td:eq(2)').text()); 
+ 
+});
+
+$(document).on("click", ".btnRemove", function(event) 
+{ 
+ $.ajax( 
+ { 
+ url : "ElectricityAccAPI", 
+ type : "DELETE", 
+ data : "accountID=" + $(this).data("accountID"),
+ dataType : "text", 
+ complete : function(response, status) 
+ { 
+ onAccountDeleteComplete(response.responseText, status); 
+ } 
+ }); 
+});
+// CLIENT-MODEL================================================================
+function validateItemForm() 
+{ 
+// NAME
+if ($("#accName").val().trim() == "") 
+ { 
+ return "Insert Account Name."; 
+ } 
+// NUMBER
+if ($("#accNumber").val().trim() == "") 
+ { 
+ return "Insert Account Number."; 
+ } 
+// PREMISES ID------------------------
+if ($("#premisesID").val().trim() == "") 
+ { 
+ return "Insert Premises ID."; 
+ } 
+return true; 
+}
+
+function onAccountSaveComplete(response, status) 
+{ 
+if (status == "success") 
+ { 
+ var resultSet = JSON.parse(response); 
+ if (resultSet.status.trim() == "success") 
+ { 
+ $("#alertSuccess").text("Successfully saved."); 
+ $("#alertSuccess").show(); 
+ $("#divItemsGrid").html(resultSet.data); 
+ } else if (resultSet.status.trim() == "error") 
+ { 
+ $("#alertError").text(resultSet.data); 
+ $("#alertError").show(); 
+ } 
+ } else if (status == "error") 
+ { 
+ $("#alertError").text("Error while saving."); 
+ $("#alertError").show(); 
+ } else
+ { 
+ $("#alertError").text("Unknown error while saving.."); 
+ $("#alertError").show(); 
+ } 
+ $("#hidItemIDSave").val(""); 
+ $("#formItem")[0].reset(); 
+}
+
+
+function onAccountDeleteComplete(response, status) 
+{ 
+if (status == "success") 
+ { 
+ var resultSet = JSON.parse(response); 
+ if (resultSet.status.trim() == "success") 
+ { 
+ $("#alertSuccess").text("Successfully deleted."); 
+ $("#alertSuccess").show(); 
+ $("#divItemsGrid").html(resultSet.data); 
+ } else if (resultSet.status.trim() == "error") 
+ { 
+ $("#alertError").text(resultSet.data); 
+ $("#alertError").show(); 
+ } 
+ } else if (status == "error") 
+ { 
+ $("#alertError").text("Error while deleting."); 
+ $("#alertError").show(); 
+ } else
+ { 
+ $("#alertError").text("Unknown error while deleting.."); 
+ $("#alertError").show(); 
+ } 
+}
+
+
+
+
